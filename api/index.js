6729dee2f2df3c807e8533f401cb94f5e81d87aa
Umar-Basra <umerali786614@gmail.com>
2024-03-15 15:13:34 +0500

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,19 @@
-import express from 'express'
-import colors from 'colors'
+import express from 'express';
+import colors from 'colors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+
+mongoose.connect( process.env.MONGO)
+.then(() => {
+    console.log('mongodb is connected'.bgBlue);
+})
+.catch( (err) => {
+    console.log(`error is this ${err}`.bgRed);
+})
+
 const app = express();
 
 const PORT = 3000;
