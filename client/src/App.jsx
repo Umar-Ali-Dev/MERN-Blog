3ac5312804ec48ebf23cbf72deb2ae64fe7239a7
Umar-Basra <umerali786614@gmail.com>
2024-03-15 10:41:44 +0500

    install vite react app with tailwind css

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+ import React from 'react'
+ 
+ const App = () => {
+   return (
+     <div>
+       <h1 className='text-4xl text-red-600'>main file</h1>
+     </div>
+   )
+ }
+ 
+ export default App
+ 
