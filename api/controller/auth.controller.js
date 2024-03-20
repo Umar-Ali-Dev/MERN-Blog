c8caf60de45880b7b7fae062cdb436483eeca52c
Umar-Basra <umerali786614@gmail.com>
2024-03-20 11:25:17 +0500

    add admin functionality to the user

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -56,7 +56,7 @@ export const signin = async (req, res, next) => {
            return  next(errorHandler(400, "Invalid Password"))
         }
 
-        const token = jwt.sign({id : validUser._id},process.env.JWT_SECRET )
+        const token = jwt.sign({id : validUser._id, isAdmin : validUser.isAdmin},process.env.JWT_SECRET )
 
         // different approach start 
 
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -20,6 +20,10 @@ const userSchema = new mongoose.Schema({
     },
     userToken :{
         type : String
+    },
+    isAdmin:{
+        type : Boolean,
+        default : false
     }
 }, {timestamps : true});
 const User = mongoose.model('user', userSchema)
